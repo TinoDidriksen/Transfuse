64c8af44de0ed1e31f5985994708421efd4ecdad
Tino Didriksen <mail@tinodidriksen.com>
2022-12-30 00:22:01 +0100

    WIP remove ODT fluff and coalesce identical styles (issue #23)

--- src/format-odt.cpp
+++ src/format-odt.cpp
@@ -19,9 +19,21 @@
 #include "formats.hpp"
 #include <libxml/tree.h>
 #include <libxml/xmlsave.h>
+#include <unicode/ustring.h>
+#include <unicode/regex.h>
 #include <zip.h>
+#include <unordered_map>
+using namespace icu;
 
 namespace Transfuse {
+
+struct ustring_hash {
+	using hash_type = std::hash<ustring_view>;
+	using is_transparent = void;
+	size_t operator()(const UChar* str) const { return hash_type{}(str); }
+	size_t operator()(ustring_view str) const { return hash_type{}(str); }
+	size_t operator()(UnicodeString const& str) const { return hash_type{}(ustring_view(str.getBuffer(), str.length())); }
+};
 
 std::unique_ptr<DOM> extract_odt(State& state) {
 	int e = 0;
@@ -51,7 +63,63 @@ std::unique_ptr<DOM> extract_odt(State& state) {
 
 	// ToDo: Turn <text:tab> and <text:tab [^>]*> into \t?
 
-	auto xml = xmlReadMemory(data.c_str(), SI(data.size()), "content.xml", "UTF-8", XML_PARSE_RECOVER | XML_PARSE_NONET);
+	UnicodeString tmp;
+	auto udata = UnicodeString::fromUTF8(data);
+	udata.findAndReplace(" encoding=\"UTF-8\"", " encoding=\"UTF-16\"");
+
+	rx_replaceAll(R"X( fo:language="[^"]+")X", "", udata, tmp);
+	rx_replaceAll(R"X( style:language-complex="[^"]+")X", "", udata, tmp);
+	rx_replaceAll(R"X( style:language-asian="[^"]+")X", "", udata, tmp);
+	rx_replaceAll(R"X( fo:country="[^"]+")X", "", udata, tmp);
+	rx_replaceAll(R"X( style:country-complex="[^"]+")X", "", udata, tmp);
+	rx_replaceAll(R"X( style:country-asian="[^"]+")X", "", udata, tmp);
+
+	UnicodeString normed = udata;
+	UnicodeString rpl;
+	std::unordered_map<UnicodeString, UnicodeString, ustring_hash> styles;
+	UErrorCode status = U_ZERO_ERROR;
+	RegexMatcher rx_styles(R"X((<style:style style:name=")([^"]+)(".+?</style:style>))X", 0, status);
+
+	rx_styles.reset(udata);
+	while (rx_styles.find()) {
+		tmp.remove();
+
+		auto b1 = rx_styles.start(1, status);
+		auto e1 = rx_styles.end(1, status);
+		tmp.append(udata, b1, e1 - b1);
+
+		auto b3 = rx_styles.start(3, status);
+		auto e3 = rx_styles.end(3, status);
+		tmp.append(udata, b3, e3 - b3);
+
+		// If the style, minus the unique name, is identical to an already seen style, replace it with the existing one
+		auto it = styles.find(tmp);
+		if (it != styles.end()) {
+			auto b0 = rx_styles.start(0, status);
+			auto e0 = rx_styles.end(0, status);
+			tmp.setTo(udata, b0, e0 - b0);
+			normed.findAndReplace(tmp, "");
+
+			auto b2 = rx_styles.start(2, status);
+			auto e2 = rx_styles.end(2, status);
+			tmp.setTo(" text:style-name=\"");
+			tmp.append(udata, b2, e2 - b2);
+			tmp.append("\"");
+			rpl.setTo(" text:style-name=\"");
+			rpl.append(it->second);
+			rpl.append("\"");
+			normed.findAndReplace(tmp, rpl);
+		}
+		else {
+			auto b2 = rx_styles.start(2, status);
+			auto e2 = rx_styles.end(2, status);
+			styles[tmp].setTo(udata, b2, e2 - b2);
+		}
+	}
+
+	udata.swap(normed);
+
+	auto xml = xmlReadMemory(reinterpret_cast<const char*>(udata.getTerminatedBuffer()), SI(SZ(udata.length()) * sizeof(UChar)), "content.xml", utf16_native, XML_PARSE_RECOVER | XML_PARSE_NONET);
 	if (xml == nullptr) {
 		throw std::runtime_error(concat("Could not parse content.xml: ", xmlLastError.message));
 	}
