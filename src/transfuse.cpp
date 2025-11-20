TinoDidriksen/Transfuse

commit 6c5bab215b58dcef3e2febbb131710bb02d6d354

Add --apertium-n (closes #9)

src/transfuse.cpp
@@ -33,8 +33,8 @@ using namespace icu;
 
 namespace Transfuse {
 
-fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stream stream, bool wipe, bool mark_headers = false);
-std::pair<fs::path, std::string> inject(fs::path tmpdir, std::istream& in, Stream stream);
+void extract(Settings&);
+std::pair<fs::path, std::string> inject(Settings&);
 
 std::istream* read_or_stdin(const char* arg, std::unique_ptr<std::istream>& in) {
 	if (arg[0] == '-' && arg[1] == 0) {
@@ -88,6 +88,7 @@ int main(int argc, char* argv[]) {
 		O('o',  "output", ARG_REQ, "output file, if not passed as arg; default and - is stdout"),
 		O('H', "mark-headers", ARG_NO, "output U+2761 after headers, such as HTML tags h1-h6 and attribute 'title'"),
 		O('V', "version",  ARG_NO, "output version information"),
+		O(0,   "apertium-n", ARG_NO, "apertium -n mode to prevent appending .[] to blocks"),
 		// Options after final() are still usable, but not shown in --help
 		final(),
 		O(0,  "url64", ARG_REQ, "base64-url encodes the passed value"),
@@ -126,90 +127,85 @@ int main(int argc, char* argv[]) {
 		return 0;
 	}
 
-	std::string_view mode{ "clean" };
+	Settings settings;
 	if (exe == "tf-extract") {
-		mode = "extract";
+		settings.mode = "extract";
 	}
 	else if (exe == "tf-inject") {
-		mode = "inject";
+		settings.mode = "inject";
 	}
 	else if (exe == "tf-clean") {
-		mode = "clean";
+		settings.mode = "clean";
 	}
-
-	std::string_view format{"auto"};
-	Stream stream{ Streams::detect };
-	fs::path tmpdir;
-
-	fs::path infile;
-	std::ostream* out = nullptr;
-	std::unique_ptr<std::ostream> _out;
 
 	// Handle cmdline arguments
 	while (auto o = opts.get()) {
 		switch (o->opt) {
 		case 'f':
-			format = o->value;
+			settings.format = o->value;
 			break;
 		case 's':
 			if (o->value == Streams::apertium) {
-				stream = Streams::apertium;
+				settings.stream = Streams::apertium;
 			}
 			else if (o->value == Streams::visl) {
-				stream = Streams::visl;
+				settings.stream = Streams::visl;
 			}
 			else if (o->value == Streams::cg) {
-				stream = Streams::cg;
+				settings.stream = Streams::cg;
 			}
 			break;
 		case 'm':
-			mode = o->value;
+			settings.mode = o->value;
 			break;
 		case 'd':
-			tmpdir = path(o->value);
+			settings.tmpdir = path(o->value);
 			opts.set("keep");
+			settings.opt_keep = true;
 			break;
 		case 'K':
 			opts.unset("keep");
+			settings.opt_keep = false;
+			settings.opt_no_keep = true;
 			break;
 		case 'i':
-			infile = path(o->value);
+			settings.infile = path(o->value);
 			break;
 		case 'o':
-			out = write_or_stdout(o->value.data(), _out);
+			settings.out = write_or_stdout(o->value.data(), settings._out);
 			break;
-		case 'D':
-			opts.set("verbose");
-			break;
+		}
+		if (o->longopt == "apertium-n") {
+			settings.opt_apertium_n = true;
 		}
 	}
 
 	// Funnel remaining unparsed arguments into input and/or output files
 	if (argc > 2) {
-		if (infile.empty() && !out) {
-			infile = argv[1];
-			out = write_or_stdout(argv[2], _out);
+		if (settings.infile.empty() && !settings.out) {
+			settings.infile = argv[1];
+			settings.out = write_or_stdout(argv[2], settings._out);
 		}
-		else if (infile.empty()) {
-			infile = argv[1];
+		else if (settings.infile.empty()) {
+			settings.infile = argv[1];
 		}
-		else if (!out) {
-			out = write_or_stdout(argv[1], _out);
+		else if (!settings.out) {
+			settings.out = write_or_stdout(argv[1], settings._out);
 		}
 	}
 	else if (argc > 1) {
-		if (infile.empty()) {
-			infile = argv[1];
+		if (settings.infile.empty()) {
+			settings.infile = argv[1];
 		}
-		else if (!out) {
-			out = write_or_stdout(argv[2], _out);
+		else if (!settings.out) {
+			settings.out = write_or_stdout(argv[2], settings._out);
 		}
 	}
-	if (infile.empty()) {
-		infile = "-";
+	if (settings.infile.empty()) {
+		settings.infile = "-";
 	}
-	if (!out) {
-		out = &std::cout;
+	if (!settings.out) {
+		settings.out = &std::cout;
 	}
 
 	UErrorCode status = U_ZERO_ERROR;
@@ -219,40 +215,38 @@ int main(int argc, char* argv[]) {
 	}
 
 	auto curdir = fs::current_path();
-	std::istream* in = nullptr;
-	std::unique_ptr<std::istream> _in;
 
-	if (mode == "clean") {
+	if (settings.mode == "clean") {
 		// Extracts and immediately injects again - useful for cleaning documents for other CAT tools, such as OmegaT
-		tmpdir = extract(tmpdir, infile, format, stream, opts["no-keep"] != nullptr, opts["mark-headers"] != nullptr);
-		in = read_or_stdin("extracted", _in);
-		auto rv = inject(tmpdir, *in, stream);
+		extract(settings);
+		settings.in = read_or_stdin("extracted", settings._in);
+		auto rv = inject(settings);
 		std::ifstream data(rv.second, std::ios::binary);
 		data.exceptions(std::ios::badbit | std::ios::failbit);
-		(*out) << data.rdbuf();
-		out->flush();
-		tmpdir = rv.first;
+		(*settings.out) << data.rdbuf();
+		settings.out->flush();
+		settings.tmpdir = rv.first;
 	}
-	else if (mode == "extract") {
-		tmpdir = extract(tmpdir, infile, format, stream, opts["no-keep"] != nullptr, opts["mark-headers"] != nullptr);
+	else if (settings.mode == "extract") {
+		extract(settings);
 		std::ifstream data("extracted", std::ios::binary);
 		data.exceptions(std::ios::badbit | std::ios::failbit);
-		(*out) << data.rdbuf();
-		out->flush();
+		(*settings.out) << data.rdbuf();
+		settings.out->flush();
 	}
-	else if (mode == "inject") {
-		in = read_or_stdin(infile, _in);
-		auto rv = inject(tmpdir, *in, stream);
+	else if (settings.mode == "inject") {
+		settings.in = read_or_stdin(settings.infile, settings._in);
+		auto rv = inject(settings);
 		std::ifstream data(rv.second, std::ios::binary);
 		data.exceptions(std::ios::badbit | std::ios::failbit);
-		(*out) << data.rdbuf();
-		out->flush();
-		tmpdir = rv.first;
+		(*settings.out) << data.rdbuf();
+		settings.out->flush();
+		settings.tmpdir = rv.first;
 	}
 
 	// If neither --dir nor --keep, wipe the temporary folder
-	if (!opts["keep"] && (mode == "clean" || mode == "inject")) {
+	if (!settings.opt_keep && (settings.mode == "clean" || settings.mode == "inject")) {
 		fs::current_path(curdir);
-		fs::remove_all(tmpdir);
+		fs::remove_all(settings.tmpdir);
 	}
 }

CMakeLists.txt
@@ -1,7 +1,7 @@
 cmake_minimum_required(VERSION 3.12 FATAL_ERROR)
 cmake_policy(VERSION ${CMAKE_MAJOR_VERSION}.${CMAKE_MINOR_VERSION})
 project(transfuse
-	VERSION 0.6.4
+	VERSION 0.6.5
 	LANGUAGES CXX C
 	)
 

src/dom.cpp
@@ -80,10 +80,10 @@ DOM::DOM(State& state, xmlDocPtr xml)
   , rx_any_alnum(UnicodeString::fromUTF8(R"X([\w\p{L}\p{N}\p{M}])X"), 0, status)
 {
 	if (state.stream() == Streams::apertium) {
-		stream.reset(new ApertiumStream);
+		stream.reset(new ApertiumStream(state.settings));
 	}
 	else {
-		stream.reset(new VISLStream);
+		stream.reset(new VISLStream(state.settings));
 	}
 
 	if (U_FAILURE(status)) {

src/dom.hpp
@@ -194,7 +194,7 @@ struct DOM {
 	void extract_blocks(xmlString&, xmlNodePtr, size_t, bool txt = false, bool header = false);
 	xmlString extract_blocks() {
 		xmlString rv;
-		stream->stream_header(rv, state.tmpdir);
+		stream->stream_header(rv, state.settings->tmpdir);
 		blocks = 0;
 		extract_blocks(rv, reinterpret_cast<xmlNodePtr>(xml.get()), 0);
 		return rv;

src/extract.cpp
@@ -32,7 +32,13 @@
 
 namespace Transfuse {
 
-fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stream stream, bool wipe, bool mark_headers) {
+void extract(Settings& settings) {
+	fs::path& tmpdir = settings.tmpdir;
+	fs::path& infile = settings.infile;
+	std::string_view& format = settings.format;
+	Stream& stream = settings.stream;
+	bool& wipe = settings.opt_no_keep;
+
 	if (stream == Streams::detect) {
 		stream = Streams::apertium;
 	}
@@ -116,7 +122,7 @@ fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stre
 
 		fs::current_path(tmpdir);
 
-		state = std::make_unique<State>(fs::current_path());
+		state = std::make_unique<State>(&settings);
 		state->name(infile.filename().string());
 
 		if (format == "auto") {
@@ -201,7 +207,6 @@ fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stre
 
 		state->format(format);
 		state->stream(stream);
-		state->opt_mark_headers = mark_headers;
 
 		if (format == "docx") {
 			dom = extract_docx(*state);
@@ -238,7 +243,7 @@ fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stre
 		if (xml == nullptr) {
 			throw std::runtime_error(concat("Could not parse styled.xml: ", xmlLastError.message));
 		}
-		state = std::make_unique<State>(fs::current_path(), true);
+		state = std::make_unique<State>(&settings, true);
 		dom = std::make_unique<DOM>(*state, xml);
 	}
 
@@ -248,8 +253,6 @@ fs::path extract(fs::path tmpdir, fs::path infile, std::string_view format, Stre
 	auto cntx = xmlSaveToFilename("content.xml", "UTF-8", 0);
 	xmlSaveDoc(cntx, dom->xml.get());
 	xmlSaveClose(cntx);
-
-	return tmpdir;
 }
 
 }

src/format-html.cpp
@@ -138,7 +138,7 @@ std::unique_ptr<DOM> extract_html(State& state, std::unique_ptr<icu::UnicodeStri
 	dom->tags_raw = make_xmlChars("script", "style", "svg");
 	dom->tags_inline = make_xmlChars("a", "abbr", "acronym", "address", "b", "bdi", "bdo", "big", "del", "em", "font", "i", "ins", "kbd", "mark", "meter", "output", "q", "s", "samp", "small", "span", "strike", "strong", "sub", "sup", "time", "tt", "u", "var");
 	dom->tag_attrs = make_xmlChars("alt", "caption", "label", "summary", "title", "placeholder");
-	if (state.opt_mark_headers) {
+	if (state.settings->opt_mark_headers) {
 		dom->tags_headers = make_xmlChars("h1", "h2", "h3", "h4", "h5", "h6");
 		dom->attr_headers = make_xmlChars("title");
 	}

src/inject.cpp
@@ -30,7 +30,11 @@ using namespace icu;
 
 namespace Transfuse {
 
-std::pair<fs::path,std::string> inject(fs::path tmpdir, std::istream& in, Stream stream) {
+std::pair<fs::path,std::string> inject(Settings& settings) {
+	fs::path& tmpdir = settings.tmpdir;
+	std::istream& in = *settings.in;
+	Stream& stream = settings.stream;
+
 	std::ios::sync_with_stdio(false);
 	in.tie(nullptr);
 
@@ -45,23 +49,23 @@ std::pair<fs::path,std::string> inject(fs::path tmpdir, std::istream& in, Stream
 
 	if (stream == Streams::detect) {
 		if (buffer.find("[transfuse:") != std::string::npos) {
-			sformat.reset(new ApertiumStream);
+			sformat.reset(new ApertiumStream(&settings));
 		}
 		else if (buffer.find("<STREAMCMD:TRANSFUSE:") != std::string::npos) {
-			sformat.reset(new VISLStream);
+			sformat.reset(new VISLStream(&settings));
 		}
 		else {
 			throw std::runtime_error("Could not detect input stream format");
 		}
 	}
 	else if (stream == Streams::apertium) {
-		sformat.reset(new ApertiumStream);
+		sformat.reset(new ApertiumStream(&settings));
 	}
 	else if (stream == Streams::cg) {
-		sformat.reset(new CGStream);
+		sformat.reset(new CGStream(&settings));
 	}
 	else {
-		sformat.reset(new VISLStream);
+		sformat.reset(new VISLStream(&settings));
 	}
 
 	if (tmpdir.empty()) {
@@ -145,7 +149,7 @@ std::pair<fs::path,std::string> inject(fs::path tmpdir, std::istream& in, Stream
 
 	cleanup_styles(content);
 
-	State state(fs::current_path(), true);
+	State state(&settings, true);
 
 	UText tmp_ut = UTEXT_INITIALIZER;
 	UErrorCode status = U_ZERO_ERROR;

src/shared.hpp
@@ -266,6 +266,33 @@ std::string detect_encoding(std::string_view);
 
 icu::UnicodeString to_ustring(std::string_view, std::string_view);
 
+namespace Streams {
+	const std::string_view detect{ "detect" };
+	const std::string_view apertium{ "apertium" };
+	const std::string_view visl{ "visl" };
+	const std::string_view cg{ "cg" };
+}
+using Stream = std::string_view;
+
+struct Settings {
+	std::string_view mode{ "clean" };
+	std::string_view format{ "auto" };
+	Stream stream{ Streams::detect };
+
+	fs::path tmpdir;
+	fs::path infile;
+	std::istream* in = nullptr;
+	std::unique_ptr<std::istream> _in;
+	std::ostream* out = nullptr;
+	std::unique_ptr<std::ostream> _out;
+
+	bool opt_debug = false;
+	bool opt_keep = false;
+	bool opt_no_keep = false;
+	bool opt_mark_headers = false;
+	bool opt_apertium_n = false;
+};
+
 }
 
 #endif

commit 6550b911668c91900a188a298f7790b655cc0a45

Fix -H (broken by commit 6c5bab215b58dcef3e2febbb131710bb02d6d354) (see #15)

src/transfuse.cpp
@@ -228,6 +228,9 @@ int main(int argc, char* argv[]) {
 		else if (o->longopt == "debug") {
 			settings.opt_debug = true;
 		}
+		else if (o->longopt == "mark-headers") {
+			settings.opt_mark_headers = true;
+		}
 	}
 
 	for (auto mt : maybe_tags) {